e5e4e2a6fcf14f7e9a4d8e9754acfaed2a451f35
Gman <mrgman11@yandex.ru>
2020-04-07 08:55:58 +0300

    logical fixes

--- dz_orm.py
+++ dz_orm.py
@@ -11,64 +11,52 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     login = Column(String(40), nullable=False)
-    password = Column(String(40), nullable=False)
     email = Column(String(40), nullable=False)
-
-    person = relationship("Person", back_populates="user")
-
-
-class Person(Base):
-    __tablename__ = 'person'
-
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey(User.id), nullable=False)
-    fio = Column(String(40), nullable=False)
+    last_first_name_patronymic = Column(String(40), nullable=False)
     date_birth = Column(String(12), nullable=True)
-    type_person = Column(String(22), nullable=False)
-    # type_person: 1)amateur ,2)fitness bloger
-
-    user = relationship("User", back_populates="person")
-    amateur = relationship("Amateur", back_populates="person")
-    fitness_blogger = relationship("FitnessBlogger", back_populates="person")
+    type_user = Column(String(22), nullable=False)
+    amateur = relationship("Amateur", back_populates="user")
+    fitness_blogger = relationship("FitnessBlogger", back_populates="user")
 
 
 class Amateur(Base):
     __tablename__ = 'amateur'
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey(Person.user_id), nullable=False)
-    cumir = Column(String(22), nullable=False)
+    user_id = Column(Integer, ForeignKey(User.id), nullable=False)
+    idol = Column(String(22), nullable=False)
     sport_kind = Column(String(22), nullable=False)
 
-    person = relationship("Person", back_populates="amateur")
+    user = relationship("User", back_populates="amateur")
 
 
 class FitnessBlogger(Base):
     __tablename__ = 'fitness_blogger'
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey(Person.user_id), nullable=False)
-    cumir = Column(String(22), nullable=False)
-    sport_kind = Column(String(22), nullable=False)
+    user_id = Column(Integer, ForeignKey(User.id), nullable=False)
+    link_page = Column(String(22), nullable=False)
+    contact = Column(String(22), nullable=False)
 
-    person = relationship("Person", back_populates="fitness_blogger")
+    user = relationship("User", back_populates="fitness_blogger")
     # contacts: 1)tel_number ,2)email
 
 
 def create_user_amateur():
     session = Session()
 
-    user = User(login="test_user", password="test_pswd", email="testmail@rambler.ru")
+    user = User(login="test_user", email="testmail@rambler.ru", last_first_name_patronymic="ivanov ivan ivanovich", type_user="amateur")
     session.add(user)
     session.flush(session)
 
-    person = Person(user_id=1, fio="ivanov ivan ivanovich", type_person="amateur")
-    session.add(person)
+    fitness_blogger = FitnessBlogger(user_id=1, link_page="http://sports.ru", contact="88005553535")
+    session.add(fitness_blogger)
     session.flush(session)
 
-    amateur = Amateur(user_id=1, cumir="floyd mayweather", sport_kind="box")
+    amateur = Amateur(user_id=1, idol="floyd mayweather", sport_kind="box")
     session.add(amateur)
     session.flush(session)
+
     session.commit()
     session.close()
 
